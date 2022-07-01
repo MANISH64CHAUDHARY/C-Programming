726d6c185ad9590283b37af0b7a07c68cb2eee16
MANISH64CHAUDHARY <99059785+MANISH64CHAUDHARY@users.noreply.github.com>
2022-07-01 14:00:20 +0545

    Update ARITHMETIC OPERATORS.C

--- ARITHMETIC OPERATORS.C
+++ ARITHMETIC OPERATORS.C
@@ -9,7 +9,7 @@ void main()              //tell the compiler the start of the program
     printf("sum=%d",sum);       //display the output
     sub=num1-num2;                     //subtraction of numbers and storing in sub.
     printf("difference=%d",sub);   //display the output
-    mul=num1*num2;             /multiplication of numbers and storing in mul.
+    mul=num1*num2;             //multiplication of numbers and storing in mul.
     printf("product is=%d",mul);      //display the output
     div=num1/num2;                           //division of numbers and storing in div.
     printf("division=%d",div);        //display the output
