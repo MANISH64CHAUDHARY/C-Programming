70313ed8affe9eaaf52f45b773b62ba88e7a912a
MANISH64CHAUDHARY <99059785+MANISH64CHAUDHARY@users.noreply.github.com>
2022-07-01 09:50:37 +0545

    Update PROGRAM TO FIND SUM OF ANY TWO NUMBERS.C

--- PROGRAM TO FIND SUM OF ANY TWO NUMBERS.C
+++ PROGRAM TO FIND SUM OF ANY TWO NUMBERS.C
@@ -1,18 +1,18 @@
-/*program to find sum of any two numbers*/
+/*program to find sum of any two numbers*/...............>/*Comments*/
 #include<stdio.h>............>Header files
 #include <conio.h>...........>Header files
-    int sum(int, int)
-    int s;
-    void main()
+    int sum(int, int)...............>Function header
+    int s;....................>Global Declaration
+    void main()..........>main()
     {
-        int a, b;
-                  printf("Enter any two numbers:");
-                         scanf("%d%d", &a, &b);
-                         s = sum(a, b);
-                        printf("sum=%d", s);
-              getch();
+        int a, b;..............>Declaration part
+                  printf("Enter any two numbers:");.........>Executable part
+                         scanf("%d%d", &a, &b);...........>Executable part
+                         s = sum(a, b);..................>Executable part
+                        printf("sum=%d", s);...........>Executable part
+              getch();..........>Executable part
     }
-    int sum(int x,int y)
+    int sum(int x,int y)............>User defined function
     {
         int Z;
         Z = x + y;
