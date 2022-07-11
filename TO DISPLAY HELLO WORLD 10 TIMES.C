a1a331ac935c7c2fda62be7a3c15f53745dee51e
MANISH64CHAUDHARY <99059785+MANISH64CHAUDHARY@users.noreply.github.com>
2022-07-11 16:46:00 +0545

    Add files via upload

---
+++ TO DISPLAY HELLO WORLD 10 TIMES.C
@@ -0,0 +1,15 @@
+/*To display Hello World 10 times*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int c;
+    c = 1;
+    do
+    {      printf("Hello World\n");
+    c++;
+
+
+    }      while (c <= 10);
+    getch();
+}
