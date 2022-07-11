7d34c3a555faf43e28bfe34dd48e1523c314cb86
MANISH64CHAUDHARY <99059785+MANISH64CHAUDHARY@users.noreply.github.com>
2022-07-11 16:06:53 +0545

    Add files via upload

---
+++ PRINTS HELLO WORLD 10 TIMES.C
@@ -0,0 +1,30 @@
+/* Prints hello world 10 times */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int c
+    c= 1;                                             /* initialization */
+    while (c <= 10)                                   /* repetition condition */
+    {
+        printf("Hello World\n");
+        ++c;                                          /* increment */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    getch()
+}
