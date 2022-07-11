2f48474c7140b610ed7384c5f45ff2f79b2979b6
MANISH64CHAUDHARY <99059785+MANISH64CHAUDHARY@users.noreply.github.com>
2022-07-11 19:52:42 +0545

    Add files via upload

---
+++ PRINTS HELLO WORLD 10 TIMES ON THE SCREEN.C
@@ -0,0 +1,12 @@
+/* Prints Hello World 10 times on the screen */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int c;
+    for (c = 1;c<=10;c++)
+    {
+        printf("Hello World\n");
+    }
+    getch();
+}
